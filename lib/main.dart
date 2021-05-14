cbadd7be63820f9fc8f8c4a5c4c3836f5fd24fb1
jemin <rudanijemin18@gmail.com>
2021-05-14 10:53:01 +0530

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("hello word"),
+      home: Scaffold(
+        backgroundColor: Colors.cyanAccent,
+        appBar: AppBar(
+          title: Text("I AM RICH"),
+          backgroundColor: Colors.blueGrey[900],
+
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://imgd.aeplcdn.com/476x268/n/cw/ec/38904/mt-15-front-view.jpeg?q=80'),
+          ),
+        ),
       ),
     ),
   );
